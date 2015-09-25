43d39d6b4fe67e26836c513c917081b0871c8d5c
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-09-25 19:01:40 -0300

    fix(completions.pip): remove a dupe function

--- completions/pip.fish
+++ completions/pip.fish
@@ -8,23 +8,6 @@ function __fish_pip_needs_command
   return 1
 end
 
-
-function __fish_pip_using_command
-  set cmd (commandline -opc)
-  if [ (count $cmd) -gt 1 ]
-    if [ $argv[1] = $cmd[2] ]
-      return 0
-    end
-
-    # aliased command
-    set -l aliased (command git config --get "alias.$cmd[2]" ^ /dev/null | sed "s/ .*\$//")
-    if [ $argv[1] = "$aliased" ]
-      return 0
-    end
-  end
-  return 1
-end
-
 function __fish_pip_using_command
   set cmd (commandline -opc)
   if [ (count $cmd) -gt 1 ]
